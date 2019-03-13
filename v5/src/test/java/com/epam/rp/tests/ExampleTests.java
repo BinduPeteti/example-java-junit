cb777caaf69cde2bd7fa15a3e825f39bcb17b67e
Aliaksandr Sanarau <Aliaksandr_Sanarau@epam.com>
2019-03-13 11:04:46 +0300

    fix test example

---
+++ v5/src/test/java/com/epam/rp/tests/ExampleTests.java
@@ -0,0 +1,80 @@
+package com.epam.rp.tests;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ExampleTests {
+
+    @BeforeAll
+    static void setup() {
+        log.info("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void init() {
+        log.info("@BeforeEach - executes before each test method in this class");
+    }
+
+    @DisplayName("Single test successful")
+    @Test
+    void testSingleSuccessTest() {
+        log.info("Success");
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void testShowSomething() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        log.info("@AfterEach - executed after each test method.");
+    }
+
+    @AfterAll
+    static void done() {
+        log.info("@AfterAll - executed after all test methods.");
+    }
+
+    @BeforeEach
+    void initializeBaseClass() {
+        log.info("Before each");
+    }
+
+    @Test
+    @Tag("tag1")
+    @Tag("tag2")
+    void baseClassTest() {
+        log.info("test");
+    }
+
+    @Nested
+    class FirstContext {
+
+        @BeforeEach
+        void initializeFirstNesting() {
+            log.info("nested - before each");
+        }
+
+        @RepeatedTest(10)
+        void firstNestedTest() {
+            log.info("nested - test");
+        }
+
+        @AfterEach
+        void afterFirstContext() {
+            log.info("nested - after each");
+        }
+    }
+}
